14d751a369e34f59cf2519f5863a6de48ab8c417
jeonghi <jpark0902@kookmin.ac.kr>
2022-07-13 14:50:25 +0900

    추가: 체크박스와 라벨을 렌더링하는 컴포넌트 Todo 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Todo from "./Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//App은 컴포넌트이다.
+//컴포넌트는 js함수 또는 js클래스 형태로 생성할 수 있다.
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+//App 컴포넌트는 HTML(렌더링 부분) 과 JS(로직 부분)를 포함하는 JSX를 리턴한다.
+//js파일안에 js문법과 html코드를 사용하고 있다. 이는 리엑트가 한 파일에서 HTML과 js를 함께 쓰려고 확장한 문법이고 JSX라고 부른다.
+//App 컴포넌트의 클래스 버전이다.
+class App extends React.Component {
+  render(){
+    return (
+        <div className="App">
+            <Todo/>
+        </div>
+    )
+  }
 }
-
+// export default 컴포넌트 구문을 통해 이 컴포넌트를 다른 컴포넌트에서 사용할 수 있게된다.
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root 엘리먼트가 최상위가 된다.
+
+//ReactDOM 이 내부의 컴포넌트들을 'root' 엘리먼트에 렌더한다.
+
+// 1. import 를 이용해 App 컴포넌트를 불러온다.
+// 2. <컴포넌트이름/> 을 이용해 컴포넌트를 사용한다.
+
+// root 엘리먼트 아래에 컴포넌트를 추가한다.
 root.render(
   <React.StrictMode>
     <App />
