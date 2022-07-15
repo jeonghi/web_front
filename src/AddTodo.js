db35cfcc19404ddbe41aa105d39e0a02abc4c24d
jeonghi <jpark0902@kookmin.ac.kr>
2022-07-15 21:44:17 +0900

    추가: AddTodo 컴포넌트 TextField UI 의 onKeyPress 이벤트에 이벤트 핸들러 함수 enterKeyEventHandler() 연결

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -22,6 +22,12 @@ class AddTodo extends React.Component {
         this.setState({item: {title: ""}});
     }
 
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            this.onButtonClick();
+        }
+    }
+
     render() {
         return(
             <Paper style={{margin: 16, padding: 16}}>
@@ -39,6 +45,7 @@ class AddTodo extends React.Component {
                                    fullWidth
                                    onChange={this.onInputChange}
                                    value={this.state.item.title}
+                                   onKeyPress={this.enterKeyEventHandler}
                         />
                     </Grid>
                     <Grid xs={1} md={1} item>
